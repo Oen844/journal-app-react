50a005ca9c5561d948c2d7ff667a638d5274be3e
Pau Egea <papau84@gmail.com>
2022-08-16 13:33:42 +0200

    sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,6 +1,7 @@
+import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system'
 import React from 'react'
-import { NavBar } from '../components';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidht = 4640;
 
@@ -8,13 +9,16 @@ export const JournalLayout = ({children}) => {
     return (
         <Box sx={{ display: 'flex' }}>
 
-            <NavBar drawerWidht={drawerWidht} />
+            <NavBar drawerWidht={ drawerWidht } />
+
+            <SideBar drawerWidht={ drawerWidht } />
 
             <Box
                 component='main'
                 sx={{ flexGrow: 1, p:3}}
             >
                 
+                <Toolbar />
                 {children}
 
             </Box>
