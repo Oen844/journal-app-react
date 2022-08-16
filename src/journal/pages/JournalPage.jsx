91cba39015c32efcd4a905fe91bac2332c9c3d40
Pau Egea <papau84@gmail.com>
2022-08-16 13:53:26 +0200

    nothingSelectedView no selection

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,13 +2,16 @@ import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../view/NothingSelectedView'
 
 export const JournalPage = () => {
   return (
 
     <JournalLayout>
 
-      <Typography>Adipisicing mollit aute laborum exercitation reprehenderit.</Typography>
+      {/* <Typography>Adipisicing mollit aute laborum exercitation reprehenderit.</Typography> */}
+
+      <NothingSelectedView/>
       
     </JournalLayout>
 
