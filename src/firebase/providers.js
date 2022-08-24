e9e7aff0ace312fec3e06213b22e2b3fc7ed4cfa
Pau Egea <papau84@gmail.com>
2022-08-24 09:58:38 +0200

    crear usuario conemail y password

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -39,6 +39,13 @@ export const registerUserWithEmailPassword = async ({ email, password, displayNa
         const { uid, photoURL } = resp.user;
         console.log(resp);
 
+        //Todo: actualizar el displayname en firebase
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+
     } catch (error) {
         console.log(error);
         return { ok: false, errorMessage: error.message }
