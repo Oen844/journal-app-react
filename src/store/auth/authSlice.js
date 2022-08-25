a6aed361805be225d0a510d50c8039c6f4c41aba
Pau Egea <papau84@gmail.com>
2022-08-25 16:44:43 +0200

    checking autenthication

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'not-auth', // checking, loggedIn, loggedOut
+        status: 'checking', // checking, loggedIn, loggedOut
         uid: null,
         email: null,
         displayName: null,
